a86ac9485781e1a2d0c61f72ecc59a2d2f1d4fce
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-08 19:30:06 +0300

    2

--- hw-5-t1/src/App.js
+++ hw-5-t1/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// function App() {
+
+// const ValidationMsg  = ({ val }) => {
+//   if (val >= 10) {
+//     return <h2>Grater than 10</h2>
+//   } else {
+//     return <h3>Less than 10</h3>
+//   }
+// };
+
+//   const val = 9;
+
+//   return (
+//     // <ValidationMsg val={15} />
+//     <>
+//     {val >= 10 ? <h2>Grater than 10</h2> : <h3>Less than 10</h3>}
+//     {val && <span>We have val</span>}
+//     </>
+//   );
+// }
+
+const  Tab1 = () =>  (
+  <h1>Text of Tab1</h1>
+);
+
+const  Tab2 = () =>  (
+  <h1>Text of Tab2</h1>
+);
+
+const  Tab3 = () =>  (
+  <h1>Text of Tab3</h1>
+);
+
+class App extends Component {
+  state =  {
+    activeTab: 1,
+  }
+
+  handleTab = (e) => {
+    this.setState({
+      activeTab: +e.target.getAttribute('data-name')
+    }) 
+  }
+
+  render() {
+
+    console.log(this.state);
+
+    const { activeTab } = this.state
+
+    return(
+      <>
+      <button data-name={1} onClick={this.handleTab}>Tab1</button>
+      <button data-name={2} onClick={this.handleTab}>Tab2</button>
+      <button data-name={3} onClick={this.handleTab}>Tab3</button>
+      {/* {activeTab === 1 && <Tab1 />}
+      {activeTab === 2 && <Tab2 />}
+      {activeTab === 3 && <Tab3 />} */}
+      {activeTab === 1 ? <Tab1 /> : activeTab === 2 ? <Tab2 /> : <Tab3 />}
+      <div>
+        {`Active tab is: ${activeTab === 1 ? 'first' : activeTab === 2 ? 'second' : 'third'}`}
+      </div>
+      </>
+    )
+  }
+
 }
 
 export default App;
